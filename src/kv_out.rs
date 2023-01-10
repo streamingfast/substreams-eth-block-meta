99f1441bf410b6948e87d9fc873b317eae10d74a
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-01-10 17:11:38 -0500

    bump 0.3.1: kv_out now encodes full protobuf as value

--- src/kv_out.rs
+++ src/kv_out.rs
@@ -1,3 +1,4 @@
+use substreams::proto;
 use substreams::{
     store::{self, DeltaProto},
     Hex,
@@ -12,52 +13,11 @@ pub fn block_meta_to_kv_ops(ops: &mut KvOperations, deltas: store::Deltas<DeltaP
     for delta in deltas.deltas {
         match delta.operation {
             Operation::Create | Operation::Update => {
-                ops.push_new(&delta.key, delta.new_value.hash.clone(), delta.ordinal);
-                ops.push_new(
-                    Hex::encode(delta.new_value.hash),
-                    delta.new_value.parent_hash.to_vec(),
-                    delta.ordinal,
-                );
+                let val = proto::encode(&delta.new_value).unwrap();
+                ops.push_new(Hex::encode(delta.new_value.hash), val, delta.ordinal);
             }
             Operation::Delete => ops.push_delete(&delta.key, delta.ordinal),
             x => panic!("unsupported opeation {:?}", x),
         }
     }
 }
-//
-//fn push_create(
-//    changes: &mut DatabaseChanges,
-//    key: &str,
-//    timestamp: BlockTimestamp,
-//    ordinal: u64,
-//    value: BlockMeta,
-//) {
-//    changes
-//        .push_change("block_meta", key, ordinal, Operation::Create)
-//        .change("at", (None, timestamp))
-//        .change("number", (None, value.number))
-//        .change("hash", (None, Hex(value.hash)))
-//        .change("parent_hash", (None, Hex(value.parent_hash)))
-//        .change("timestamp", (None, value.timestamp.unwrap()));
-//}
-//
-//fn push_update(
-//    changes: &mut DatabaseChanges,
-//    key: &str,
-//    ordinal: u64,
-//    old_value: BlockMeta,
-//    new_value: BlockMeta,
-//) {
-//    changes
-//        .push_change("block_meta", key, ordinal, Operation::Update)
-//        .change("number", (old_value.number, new_value.number))
-//        .change("hash", (Hex(old_value.hash), Hex(new_value.hash)))
-//        .change(
-//            "parent_hash",
-//            (Hex(old_value.parent_hash), Hex(new_value.parent_hash)),
-//        )
-//        .change(
-//            "timestamp",
-//            (&old_value.timestamp.unwrap(), &new_value.timestamp.unwrap()),
-//        );
-//}
